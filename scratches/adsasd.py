40f084fb5243fab18dd73e117e1f553ddc274ed6
Love1ex1111 <gitlerlove76@gmail.com>
2024-03-20 17:57:27 +0300

    + programm

--- scratches/adsasd.py
+++ scratches/adsasd.py
@@ -13,3 +13,35 @@ class Soda:
 
 soda_one = Soda()
 print(soda_one)
+
+
+
+class Car:
+    car_count = 0
+
+
+    def __init__(self, color, type, year):
+        self.color = color
+        self.type = type
+        self.year = year
+
+        Car.car_count += 1
+
+
+    def display_info(self):
+        print('Car:', self.color, self.type, self.year)
+
+
+first_car = Car(color='red', type='sport', year=2000)
+first_car.display_info()
+
+
+print(Car.car_count)
+
+second_car = Car(color='blue', type='grusovoy', year=2018)
+
+def start(self):
+    print(f'{self.type}заведена')
+
+def stop(self):
+    print(f'{self.type}остановилась')
